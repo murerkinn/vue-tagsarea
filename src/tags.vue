e719e02702f99e453505d901657c53eefe444f35
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-05-10 21:46:49 +0300

    Implement add tag functionality

--- src/tags.vue
+++ src/tags.vue
@@ -11,13 +11,27 @@ export default {
       tags: [],
     };
   },
+  methods: {
+    addTag(event) {
+      event.preventDefault(); // prevent cursor to create a new line in textarea
+
+      const tag = event.target.value.trim();
+      const existingTag = this.tags.find(t => t.toLowerCase() == tag.toLowerCase());
+
+      if (existingTag) return;
+
+      this.tags.push(tag);
+
+      event.target.value = '';
+    },
+  }
 }
 </script>
 
 <template>
   <div class="tags-container">
     <tag v-for="(tag, index) in tags" :key="index" :content="tag" />
-    <textarea class="tag-input" />
+    <textarea class="tag-input" @keydown.enter.exact="addTag" />
   </div>
 </template>
 
