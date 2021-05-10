79046a3db508a10ca4b76761b3bb353dd335acdf
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-05-10 21:31:40 +0300

    Create tags component as a wrapper for tag

---
+++ src/tags.vue
@@ -0,0 +1,42 @@
+<script>
+import Tag from './tag';
+
+export default {
+  name: 'Tags',
+  components: {
+    Tag,
+  },
+  data() {
+    return {
+      tags: [],
+    };
+  },
+}
+</script>
+
+<template>
+  <div class="tags-container">
+    <tag v-for="(tag, index) in tags" :key="index" :content="tag" />
+    <textarea class="tag-input" />
+  </div>
+</template>
+
+<style scoped>
+.tags-container {
+  border: 1px solid #ccc;
+  padding: 6px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.tags-container .tag-input {
+  outline: none;
+  border: none;
+  font-size: 16px;
+  flex-grow: 1;
+  resize: none;
+  white-space: nowrap;
+  overflow: hidden;
+  width: 40px;
+}
+</style>
