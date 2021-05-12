29f8ea99c7c4c25a0633c4451e0bffee370cbc2c
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-05-12 17:07:27 +0300

    Lint files

--- src/tags.vue
+++ src/tags.vue
@@ -30,31 +30,34 @@ export default {
     },
     theme: {
       type: String,
-      validator: (value) => ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'].indexOf(value.toLowerCase()) !== -1,
+      validator: value =>
+        ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'].indexOf(
+          value.toLowerCase()
+        ) !== -1,
     },
     placeholder: {
       type: String,
       required: false,
-      default: 'Enter your tags here...'
-    }
+      default: 'Enter your tags here...',
+    },
   },
   watch: {
     tags() {
-      this.$emit('input', (this.value && Array.isArray(this.value)) ? this.tags : this.tags.join(this.seperator));
+      this.$emit('input', this.value && Array.isArray(this.value) ? this.tags : this.tags.join(this.seperator));
     },
     existingTags(tags) {
       if (tags.length)
         setTimeout(() => {
           this.existingTags = [];
         }, 600);
-    }
+    },
   },
   methods: {
     addTag(tag) {
-      if(this.tags.find(t => t.toLowerCase() == tag.toLowerCase())) return this.existingTags.push(tag)
+      if (this.tags.find(t => t.toLowerCase() == tag.toLowerCase())) return this.existingTags.push(tag);
 
-      this.tags.push(tag)
-      this.textareaValue = ''
+      this.tags.push(tag);
+      this.textareaValue = '';
     },
     removeTag(tagIndex) {
       if (typeof tagIndex == 'number') {
@@ -76,9 +79,9 @@ export default {
 
       switch (event.type) {
         case 'keydown':
-          data = event.target.value.trim()
+          data = event.target.value.trim();
           break;
-       case 'paste':
+        case 'paste':
           data = event.clipboardData.getData('text/plain').trim();
           break;
       }
@@ -88,36 +91,46 @@ export default {
       const regex = new RegExp(this.seperator, 'g');
 
       if (this.seperator && regex.test(data)) {
-        let tags = [...(new Set(data.split(this.seperator).filter(t => t).map(t => t.trim())))];
+        let tags = [
+          ...new Set(
+            data
+              .split(this.seperator)
+              .filter(t => t)
+              .map(t => t.trim())
+          ),
+        ];
 
-        tags.forEach(this.addTag)
-      } else this.addTag(data)
+        tags.forEach(this.addTag);
+      } else this.addTag(data);
     },
     updateTag(tagIndex, event) {
       event.preventDefault();
 
-      if(tagIndex < 0) return;
+      if (tagIndex < 0) return;
 
       this.tags.splice(tagIndex, 1, event.target.textContent);
 
       this.$refs.tagsarea.focus();
-    }
+    },
   },
   created() {
-    this.tags = (this.value && Array.isArray(this.value)) ? this.value : this.value.split(this.seperator).filter((t) => t);
-  }
-}
+    this.tags = this.value && Array.isArray(this.value) ? this.value : this.value.split(this.seperator).filter(t => t);
+  },
+};
 </script>
 
 <template>
   <div class="tags-container">
-    <slot v-if="$scopedSlots.removeAllButton && tags.length > 1" name="removeAllButton" :removeAll="removeAllTags"></slot>
+    <slot
+      v-if="$scopedSlots.removeAllButton && tags.length > 1"
+      name="removeAllButton"
+      :removeAll="removeAllTags"
+    ></slot>
 
     <span v-else-if="showRemoveAll && tags.length > 1" class="remove-all-button" @click="removeAllTags">
       <span class="content">Remove All</span>
     </span>
 
-    <!-- eslint-disable -->
     <span v-if="$scopedSlots.tag">
       <slot
         v-for="(tag, index) in tags"
