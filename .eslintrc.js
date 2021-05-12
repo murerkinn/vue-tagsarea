0f7c9d2d7928bc0c0a927d6a733385ea77ebe8c4
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-05-12 16:50:00 +0300

    Install eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'vue/no-use-v-if-with-v-for': 'off',
+  },
+};
